e15811347c6850c2d9defd1f794783bda7d6fa8b
sara <sara.yasser.kamal@gmail.com>
2020-05-15 21:18:22 +0200

    fetch modified

--- processor/fetch.vhd
+++ processor/fetch.vhd
@@ -5,11 +5,11 @@ USE ieee.numeric_std.all;
 entity fetch is
     port(
         clk, rst :   in std_logic;
-        pc_in : in std_logic_vector(31 downto 0);
         curr_pc : in std_logic_vector(31 downto 0);
         R_dst, comp_logic : in std_logic_vector(31 downto 0);
         pc_out : out std_logic_vector(31 downto 0);
-        IF_ID : out std_logic_vector(47 downto 0)
+        IF_ID_instruction : out std_logic_vector(15 downto 0);
+        IF_ID_pc_incremented : out std_logic_vector(31 downto 0)
     );
 end entity;
 
@@ -36,9 +36,10 @@ architecture fetch_arc of fetch is
     component inc_dec is
         port(
             sel : std_logic;   -- 0 inc, 1 dec
+            num : std_logic;   -- 0 by 1, 1 by 2
             A: in std_logic_vector(31 downto 0);
             c: out std_logic_vector(31 downto 0)
-        );
+            );
     end component;
     
     signal instruction : std_logic_vector(15 downto 0);
@@ -49,21 +50,24 @@ architecture fetch_arc of fetch is
     begin
         inst_mem_com: inst_mem port map(clk, rst, curr_pc, instruction);
         branch_p_com: branch_p port map(clk, rst, instruction(15 downto 12), instruction(5 downto 0), curr_pc, R_dst, branch_seg);
-        inc_dec_com: inc_dec port map('0', curr_pc, pc_incremented);
+        inc_dec_com: inc_dec port map('0', '0', curr_pc, pc_incremented);
 
         pc_val <= R_dst when branch_seg = '1' 
         else comp_logic;
 
         
-        process(clk)
-        begin
+        -- process(clk)
+        -- begin
             
-            if (clk='1') then
-                IF_ID(15 downto 0) <= instruction;
-                IF_ID(47 downto 16) <= pc_incremented;
+        --     if (clk='1') then
+        --         IF_ID(15 downto 0) <= instruction;
+        --         IF_ID(47 downto 16) <= pc_incremented;
 
                 
-            end if;
-        end process;
-pc_out <= pc_val;
+        --     end if;
+        -- end process;
+        
+        IF_ID_instruction <= instruction;
+        IF_ID_pc_incremented <= pc_incremented;
+        pc_out <= pc_val;
 end architecture;
