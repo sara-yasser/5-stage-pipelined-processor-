1d8d4fb05e70b96153cff8fe2738de1633674539
sara <sara.yasser.kamal@gmail.com>
2020-05-15 21:46:47 +0200

    mem modified

--- processor/mem.vhd
+++ processor/mem.vhd
@@ -4,11 +4,22 @@ USE ieee.numeric_std.all;
 
 entity mem is
     port(
-        clk, rst :   in std_logic;
-        EX_MEM : in std_logic_vector(198 DOWNTO 0);
+        clk, rst                  :   in std_logic;
 
-        inc_sp, dec_sp : out std_logic;
-        MEM_WB : out std_logic_vector(76 downto 0)
+        EX_MEM_first_40_bits      : in std_logic_vector(39 DOWNTO 0);
+        EX_MEM_b_20_bits          : in std_logic_vector(19 downto 0);
+        EX_MEM_data_mem_in        : in std_logic_vector(31 downto 0);
+        EX_MEM_ALU_out            : in std_logic_vector(31 downto 0);
+        EX_MEM_sp                 : in std_logic_vector(31 downto 0);
+        EX_MEM_in_port_data       : in std_logic_vector(31 downto 0);
+        EX_MEM_write_back_signals : in std_logic_vector(3 downto 0);
+        EX_MEM_memory_signals     : in std_logic_vector(5 downto 0);
+
+        inc_sp, dec_sp            : out std_logic;
+        
+        MEM_WB_first_40_bits      : out std_logic_vector(39 downto 0);
+        MEM_WB_wb_result          : out std_logic_vector(31 downto 0);
+        MEM_WB_write_back_signals : out std_logic_vector(3 downto 0)
     );
 end entity;
 
@@ -43,13 +54,13 @@ architecture mem_arc of mem is
         -- memory_com: memory generic map (32, 32) port map(clk, rst, write_in_mem, data_mem_in, data_mem_out);
 
         -- initializations
-        b_20_bits <= EX_MEM(60 downto 41);
-        data_mem_in <= EX_MEM(92 downto 61);
-        ALU_out <= EX_MEM(124 downto 93);
-        sp <= EX_MEM(156 downto 125);
-        in_port_data <= EX_MEM(188 downto 157);
-        write_back_signals <= EX_MEM(192 downto 189);
-        memory_signals <= EX_MEM(198 downto 193);
+        b_20_bits          <= EX_MEM_b_20_bits;
+        data_mem_in        <= EX_MEM_data_mem_in;
+        ALU_out            <= EX_MEM_ALU_out;
+        sp                 <= EX_MEM_sp;
+        in_port_data       <= EX_MEM_in_port_data;
+        write_back_signals <= EX_MEM_write_back_signals;
+        memory_signals     <= EX_MEM_memory_signals;
 
         addr_imm <= "000000000000" & b_20_bits;
 
@@ -66,12 +77,6 @@ architecture mem_arc of mem is
         write_in_mem <= '1' when write_in_stack = '1' or MW = '1'
         else '0';
 
-        -- inc_sp_seg <= '1' when read_from_stack = '1'
-        -- else '0';
-
-        -- dec_sp_seg <= '1' when write_in_stack = '1'
-        -- else '0';
-
         inc_sp <= '1' when read_from_stack = '1'
         else '0';
 
@@ -83,25 +88,23 @@ architecture mem_arc of mem is
         else data_mem_out when WB = "10"
         else addr_imm; --when WB = "11"
 
+        MEM_WB_first_40_bits          <= EX_MEM_first_40_bits;
+        MEM_WB_wb_result              <= wb_result;
+        MEM_WB_write_back_signals     <= write_back_signals;
         
-        process(clk)
-        begin
+        -- process(clk)
+        -- begin
             
-            if rst = '1' then
-                MEM_WB <= (others => '0');
-                -- inc_sp <= '0';
-                -- dec_sp <= '0';
-            else
-                if (clk'event and clk='0') then
-                    MEM_WB(40 downto 0) <= EX_MEM(40 downto 0);
-                    MEM_WB(72 downto 41) <= wb_result;
-                    MEM_WB(76 downto 73) <= write_back_signals;
+        --     if rst = '1' then
+        --         MEM_WB <= (others => '0');
+        --     else
+        --         if (clk'event and clk='0') then
+        --             MEM_WB(40 downto 0) <= EX_MEM(40 downto 0);
+        --             MEM_WB(72 downto 41) <= wb_result;
+        --             MEM_WB(76 downto 73) <= write_back_signals;
                     
-                    -- inc_sp <= inc_sp_seg;
-                    -- dec_sp <= dec_sp_seg;
-                end if;
-            end if;
-        end process;
--- pc_out <= pc_val;
+        --         end if;
+        --     end if;
+        -- end process;
 end architecture;
 
