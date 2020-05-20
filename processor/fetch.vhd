a27411a0f4ed5b056c4ec9248cab6e09ae732676
sara <sara.yasser.kamal@gmail.com>
2020-05-21 00:35:20 +0200

    write in pc

--- processor/fetch.vhd
+++ processor/fetch.vhd
@@ -4,8 +4,8 @@ USE ieee.numeric_std.all;
 
 entity fetch is
     port(
-        clk, rst :   in std_logic;
-        data_branch : in std_logic_vector(31 downto 0);
+        clk, rst, write_in_pc :   in std_logic;
+        data_branch, write_data : in std_logic_vector(31 downto 0);
         int_address: out std_logic_vector(31 downto 0);
         R_dst : out std_logic_vector(2 downto 0);
         IF_ID_instruction : out std_logic_vector(15 downto 0);
@@ -52,20 +52,23 @@ architecture fetch_arc of fetch is
     
     signal instruction : std_logic_vector(15 downto 0);
     signal branch_seg, e : std_logic;
-    signal pc_incremented, init_pc, int_addr, curr_pc, pc_in : std_logic_vector(31 downto 0):=(others => '0');
+    signal pc_incremented, init_pc, int_addr, curr_pc, pc_in, pc_data_in : std_logic_vector(31 downto 0):=(others => '0');
     
 
     begin
         inst_mem_com: inst_mem port map(clk, rst, curr_pc, instruction, init_pc, int_addr);
         branch_p_com: branch_p port map(clk, rst, instruction(15 downto 12), instruction(5 downto 0), curr_pc, data_branch, branch_seg);
         inc_dec_com: inc_dec port map('0', '0', curr_pc, pc_incremented);
-        pc_register_com: pc_register port map(clk, rst, e, pc_in, curr_pc);
+        pc_register_com: pc_register port map(clk, rst, e, pc_data_in, curr_pc);
 
         e <= '1';
 
         -- pc_in <= init_pc when rst = '1'
         -- else data_branch when branch_seg = '1' 
         -- else comp_logic;
+
+        pc_data_in <= pc_in when write_in_pc = '0'
+        else write_data;
 
         int_address <= int_addr;
         R_dst <= instruction(11 downto 9);
