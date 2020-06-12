f020485c39abee1f89ab6867c3cffbb6e7c544f8
sara <sara.yasser.kamal@gmail.com>
2020-06-12 08:31:47 +0200

    stall in fetch

--- processor/fetch.vhd
+++ processor/fetch.vhd
@@ -4,12 +4,12 @@ USE ieee.numeric_std.all;
 
 entity fetch is
     port(
-        clk, rst, write_in_pc   : in std_logic;
-        data_branch, write_data : in std_logic_vector(31 downto 0);
-        int_address             : out std_logic_vector(31 downto 0);
-        R_dst                   : out std_logic_vector(2 downto 0);
-        IF_ID_instruction       : out std_logic_vector(15 downto 0);
-        IF_ID_pc_incremented    : out std_logic_vector(31 downto 0)
+        clk, rst, write_in_pc, read_same_inst   : in std_logic;
+        data_branch, write_data                 : in std_logic_vector(31 downto 0);
+        int_address                             : out std_logic_vector(31 downto 0);
+        R_dst                                   : out std_logic_vector(2 downto 0);
+        IF_ID_instruction                       : out std_logic_vector(15 downto 0);
+        IF_ID_pc_incremented                    : out std_logic_vector(31 downto 0)
     );
 end entity;
 
@@ -52,7 +52,7 @@ architecture fetch_arc of fetch is
     
     signal instruction : std_logic_vector(15 downto 0);
     signal branch_seg, e : std_logic;
-    signal pc_incremented, init_pc, int_addr, curr_pc, pc_in, pc_data_in : std_logic_vector(31 downto 0):=(others => '0');
+    signal pc_incremented, init_pc, int_addr, curr_pc, pc_in, pc_data_in, pc_branch_result, pc_dec : std_logic_vector(31 downto 0):=(others => '0');
     
 
     begin
@@ -60,15 +60,18 @@ architecture fetch_arc of fetch is
         branch_p_com: branch_p port map(clk, rst, instruction(15 downto 12), instruction(5 downto 0), curr_pc, data_branch, branch_seg);
         inc_dec_com: inc_dec port map('0', '0', curr_pc, pc_incremented);
         pc_register_com: pc_register port map(clk, rst, e, pc_data_in, curr_pc);
+        dec_com: inc_dec port map('1', '0', pc_in, pc_dec);
 
         e <= '1';
 
-        -- pc_in <= init_pc when rst = '1'
-        -- else data_branch when branch_seg = '1' 
-        -- else comp_logic;
 
-        pc_data_in <= pc_in when write_in_pc = '0'
+        -- muxes
+        pc_branch_result <= pc_in when write_in_pc = '0'
         else write_data;
+
+        pc_data_in <= pc_branch_result when read_same_inst = '0'
+        else pc_dec;
+
 
         int_address <= int_addr;
         R_dst <= instruction(11 downto 9);
